3e6b620596a0309d3ae62c015e2213c9f865d630
africa93 <africa_93@hotmail.com>
2015-09-29 17:57:47 +0200

    Release1.1. mB() to C31

--- Scripts/Package.bat
+++ Scripts/Package.bat
@@ -1,1 +1,17 @@
 ::Realiza un clean y test en el perfil develop y un package en el perfil preproduction
+@echo off
+set workspace=D:\master\spai\Practicas\spai1
+::Pinta por consola
+echo ----------------------------------------
+echo %workspace%
+::Me cambio a mi directorio
+D:
+cd %workspace%
+
+echo ----------------------------------------
+call mvn clean test -Denviroment.type=develop
+
+::Aqui se empaqueta en un jar
+call mvn package -Denvironment.type=preproduction
+cmd /k
+::pause
