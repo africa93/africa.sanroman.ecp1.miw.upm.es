808f7e462c9e1f4a50d2e71f369660081be9a4e4
africa93 <africa_93@hotmail.com>
2015-09-29 17:02:18 +0200

    Feature02. Test C21

---
+++ src/test/java/africa/sanroman/ecp1/miw/upm/es/C21Test.java
@@ -0,0 +1,25 @@
+package africa.sanroman.ecp1.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C21Test {
+    private C21 c;
+    
+    @Before
+    public void test(){
+        c = new C21();
+    }
+    @Test
+    public void testM1() {
+        assertEquals(c.m1(), "m1");
+    }
+
+    @Test
+    public void testM2() {
+        assertEquals(c.m2(), "m2");
+    }
+
+}
